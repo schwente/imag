2ea548b397d2e5339f0f9f99a9f61ddf078cea34
Matthias Beyer <mail@beyermatthias.de>
2016-02-15 13:29:58 +0100

    internal fixup: Trait docu

--- libimaglink/src/internal.rs
+++ libimaglink/src/internal.rs
@@ -10,12 +10,16 @@ use toml::Value;
 
 pub trait InternalLinker {
 
+    /// Get the internal links from the implementor object
     fn get_internal_links(&self) -> Result<Links>;
 
+    /// Set the internal links for the implementor object
     fn set_internal_links(&mut self, links: Links) -> Result<Links>;
 
+    /// Add an internal link to the implementor object
     fn add_internal_link(&mut self, link: Link) -> Result<()>;
 
+    /// Remove an internal link from the implementor object
     fn remove_internal_link(&mut self, link: Link) -> Result<()>;
 
 }
