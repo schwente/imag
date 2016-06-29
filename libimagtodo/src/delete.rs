bb67246a7b0de5a162f6e61d0d949aff61e95718
schwente <sascha.schwenteck@hs-furtwangen.de>
2016-06-29 13:32:38 +0200

    Changes

--- libimagtodo/src/delete.rs
+++ libimagtodo/src/delete.rs
@@ -3,11 +3,14 @@ use uuid::Uuid;
 use libimagstore::store::Store;
 use libimagstore::storeid::IntoStoreId;
 use module_path::ModuleEntryPath;
+use std::result::Result as RResult;
 
 use error::{TodoError, TodoErrorKind};
 
+pub type Result<T> = RResult<T, TodoError>;
+
 /// With the uuid we get the storeid and then we can delete the entry
-pub fn deleteFunc(uuid: Uuid, store : &Store) -> Result<(),TodoError> {	
+pub fn delete_func(store : &Store, uuid: Uuid) -> Result<()> {	
 	// With the uuid we get the storeid
 	let store_id = ModuleEntryPath::new(format!("taskwarrior/{}", uuid)).into_storeid();
 	// It deletes an entry	
